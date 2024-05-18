031a48799bbbd2cd4dc17671d5dcfa7e9acdc375
Kartikaypandey <kartikaypandey007@gmail.com>
2024-05-19 00:53:19 +0530

    initial commit: connection to db & entity creation

---
+++ src/main/java/com/example/pandey/bankingapp/entity/Account.java
@@ -0,0 +1,21 @@
+package com.example.pandey.bankingapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name")
+    private String userName;
+    private Double balance;
+}
